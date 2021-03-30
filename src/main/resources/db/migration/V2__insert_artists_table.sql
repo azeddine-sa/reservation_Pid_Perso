04d30b89978ac2b7f02358394f58e20cbbc0241d
Saouti Azeddine <saouti.azeddine@gmail.com>
2021-03-30 16:32:05 +0200

    Modèle Artist + migration + DONNEES INITIALES

---
+++ src/main/resources/db/migration/V2__insert_artists_table.sql
@@ -0,0 +1,14 @@
+INSERT INTO `artists` (`id`, `firstname`, `lastname`) VALUES
+(1, 'Daniel', 'Marcelin'),
+(2, 'Philippe', 'Laurent'),
+(3, 'Marius', 'Von Mayenburg'),
+(4, 'Olivier', 'Boudon'),
+(5, 'Anne Marie', 'Loop'),
+(6, 'Pietro', 'Varasso'),
+(7, 'Laurent', 'Caron'),
+(8, 'Élena', 'Perez'),
+(9, 'Guillaume', 'Alexandre'),
+(10, 'Claude', 'Semal'),
+(11, 'Laurence', 'Warin'),
+(12, 'Pierre', 'Wayburn'),
+(13, 'Gwendoline', 'Gauthier');
