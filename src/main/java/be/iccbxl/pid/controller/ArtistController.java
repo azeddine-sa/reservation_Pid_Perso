192cb9977543cef35b478f438e5084b23a63c828
Saouti Azeddine <saouti.azeddine@gmail.com>
2021-04-14 16:35:47 +0200

    Controller Artist/show + template

--- src/main/java/be/iccbxl/pid/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/controller/ArtistController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import be.iccbxl.pid.model.Artist;
 import be.iccbxl.pid.model.ArtistService;
@@ -23,6 +24,16 @@ public class ArtistController {
         model.addAttribute("title", "Liste des artistes");       
         
         return "artist/index";
+    }
+
+    @GetMapping("/artists/{id}")
+    public String show(Model model,@PathVariable("id") String id){
+        Artist artist = service.getArtist(id);
+
+        model.addAttribute("artist", artist);
+        model.addAttribute("title", "Fiche d'un artiste");
+
+        return "artist/show";
     }
 
 }
