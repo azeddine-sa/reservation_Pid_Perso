ad2d5b13aa686e8e76b113bb3d4c7a528022229b
Saouti Azeddine <saouti.azeddine@gmail.com>
2021-04-14 15:12:01 +0200

    artistcontroller avec template thymeleaf

--- src/main/java/be/iccbxl/pid/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/controller/ArtistController.java
@@ -3,28 +3,26 @@ package be.iccbxl.pid.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import be.iccbxl.pid.model.Artist;
 import be.iccbxl.pid.model.ArtistService;
 
-@RestController
+@Controller
 public class ArtistController {
     @Autowired
     ArtistService service;
 
     @GetMapping("/artists")
-    public String index(){
-        StringBuilder content = new StringBuilder("<ul>");
-
+    public String index(Model model){
         List<Artist> artists = service.getAllArtists();
-        artists.forEach(artist -> 
-            content.append("<li>"+artist+"</li>")
-        );
-        content.append("</ul>");
-    
-        return content.toString();
+
+        model.addAttribute("artists", artists);
+        model.addAttribute("title", "Liste des artistes");       
+        
+        return "artist/index";
     }
 
 }
